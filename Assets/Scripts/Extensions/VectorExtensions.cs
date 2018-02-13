d6c98aa6712d47fd8357c3aa4c6bde7bfffa80f4
Adam Ballinger <crunchienut20@gmail.com>
2018-02-13 14:14:50 +0000

    Added Vector extension class.

---
+++ Assets/Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Scripts.Extensions
+{
+    public static class VectorExtensions
+    {
+        /// <summary>
+        /// Returns the direction vector to the given vector target.
+        /// </summary>
+        /// <param name="_self"></param>
+        /// <param name="_target"></param>
+        /// <returns></returns>
+        public static Vector3 DirectionTo(this Vector3 _self, Vector3 _target)
+        {
+            return _target - _self;
+        }
+    }
+}
