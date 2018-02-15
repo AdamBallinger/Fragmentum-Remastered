0c8f95c26eefb0300026e67e7a52d12902fba1d4
Adam Ballinger <crunchienut20@gmail.com>
2018-02-15 23:27:00 +0000

    Upgraded unity to 2017.3.1f1 + base UI controller.

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.UI
+{
+    public class UIController : MonoBehaviour
+    {
+        public static UIController Instance { get; private set; }
+
+        [SerializeField]
+        [Tooltip("All objects in this list will be kept loaded through scene changes.")]
+        private List<GameObject> keepLoaded;
+
+        private void Start()
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+            
+            foreach(var obj in keepLoaded)
+            {
+                DontDestroyOnLoad(obj);
+            }
+        }
+    }
+}
