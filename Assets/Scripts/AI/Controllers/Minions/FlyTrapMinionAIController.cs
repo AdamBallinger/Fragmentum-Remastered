ee36acf6c5b28c5f3fa9436fe43d5729e841875a
Adam Ballinger <crunchienut20@gmail.com>
2018-04-13 20:02:10 +0100

    Basic fly trap minion code.

---
+++ Assets/Scripts/AI/Controllers/Minions/FlyTrapMinionAIController.cs
@@ -0,0 +1,29 @@
+﻿using Scripts.Abilities.Controllers;
+using UnityEngine;
+
+namespace Scripts.AI.Controllers.Minions
+{
+    public class FlyTrapMinionAIController : AIController
+    {
+        public float attackDelay = 1.0f;
+        public AbilityController abilityController;
+
+        private Transform player;
+
+        private void OnEnable()
+        {
+            player = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+            var sequence = new AIActionSequence(true);
+            sequence.AddActionToSequence(new IdleAction(actionManager, attackDelay));
+            sequence.AddActionToSequence(new AbilityAction(actionManager, abilityController));
+
+            actionManager.SetActionSequence(sequence);
+        }
+
+        protected override void ControllerUpdate()
+        {
+            
+        }
+    }
+}
