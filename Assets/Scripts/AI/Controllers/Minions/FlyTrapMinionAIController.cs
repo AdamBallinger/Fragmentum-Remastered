c8891eff2907d39120f1c3089e06ffe99634c47c
Adam Ballinger <crunchienut20@gmail.com>
2018-04-14 19:46:47 +0100

    Basic fly trap minion AI.

--- Assets/Scripts/AI/Controllers/Minions/FlyTrapMinionAIController.cs
+++ Assets/Scripts/AI/Controllers/Minions/FlyTrapMinionAIController.cs
@@ -5,6 +5,13 @@ namespace Scripts.AI.Controllers.Minions
 {
     public class FlyTrapMinionAIController : AIController
     {
+        [Header("Pop-out Settings.")]
+        public float popoutSpeed = 1.0f;
+        public float popoutDist = 1.0f;
+        public AnimationCurve popoutCurve;
+        private bool hasPoped;
+
+        [Header("Attack Settings.")]
         public float attackDelay = 1.0f;
         public AbilityController abilityController;
 
@@ -13,6 +20,8 @@ namespace Scripts.AI.Controllers.Minions
         private void OnEnable()
         {
             player = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+            actionManager.ToggleSequence(false);
 
             var sequence = new AIActionSequence(true);
             sequence.AddActionToSequence(new IdleAction(actionManager, attackDelay));
@@ -24,6 +33,32 @@ namespace Scripts.AI.Controllers.Minions
         protected override void ControllerUpdate()
         {
             
+        }
+
+        public void OnRadiusTriggerEnter(Collider _collider)
+        {
+            if(!hasPoped && _collider.gameObject.CompareTag("Player"))
+            {
+                hasPoped = true;
+                Animator?.SetBool("Roaring", true);
+                actionManager.SetActionImmediate(new MoveAction(actionManager, transform.position + Vector3.up * popoutDist, popoutSpeed,
+                    popoutCurve));
+            }
+        }
+
+        public override void OnManagerActionFinished(AIAction _action)
+        {
+            if(_action is MoveAction)
+            {
+                Animator?.SetBool("Roaring", false);
+                actionManager.ToggleSequence(true);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawCube(GetComponent<Transform>().position + Vector3.up * popoutDist, Vector3.one * 0.5f);
         }
     }
 }
