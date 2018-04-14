18292a9250e16622c6200227be7041e3122a1be1
Adam Ballinger <crunchienut20@gmail.com>
2018-04-14 18:10:38 +0100

    Added Trigger collider callback utility.

---
+++ Assets/Scripts/Utils/TriggerEventCallback.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scripts.Utils
+{
+    public class TriggerEventCallback : MonoBehaviour
+    {
+        [SerializeField]
+        private TriggerEvent triggerEnterEvent = null;
+
+        [SerializeField]
+        private TriggerEvent triggerStayEvent = null;
+
+        [SerializeField]
+        private TriggerEvent triggerExitEvent = null;
+
+        private void OnTriggerEnter(Collider _collider)
+        {
+            triggerEnterEvent?.Invoke(_collider);
+        }
+
+        private void OnTriggerStay(Collider _collider)
+        {
+            triggerStayEvent?.Invoke(_collider);
+        }
+
+        private void OnTriggerExit(Collider _collider)
+        {
+            triggerExitEvent?.Invoke(_collider);
+        }
+    }
+
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    { }
+}
