42e088a132b11bbede724dee51702bc63920de2e
Adam Ballinger <crunchienut20@gmail.com>
2018-04-16 10:33:05 +0100

    Added damage cooldown to HealthSystem.

--- Assets/Scripts/Combat/HealthSystem.cs
+++ Assets/Scripts/Combat/HealthSystem.cs
@@ -6,15 +6,41 @@ namespace Scripts.Combat
     {
         public int maxHealth = 100;
 
+        [Tooltip("Time in seconds between damage being registered.")]
+        public float damageCooldown = 1.0f;
+
+        private float currentCooldownTime;
+
+        private bool canTakeDamage = true;
+
         public int CurrentHealth { get; private set; }
 
         private void OnEnable()
         {
             CurrentHealth = maxHealth;
+            currentCooldownTime = 0.0f;
+            canTakeDamage = true;
+        }
+
+        private void Update()
+        {
+            if(!canTakeDamage)
+            {
+                currentCooldownTime += Time.deltaTime;
+
+                if(currentCooldownTime >= damageCooldown)
+                {
+                    currentCooldownTime = 0.0f;
+                    canTakeDamage = true;
+                }
+            }
         }
 
         public void AddHealth(int _amt)
         {
+            // prevent negative damage being added.
+            _amt = Mathf.Abs(_amt);
+
             CurrentHealth += _amt;
 
             if(CurrentHealth > maxHealth)
@@ -25,7 +51,18 @@ namespace Scripts.Combat
 
         public void RemoveHealth(int _amt)
         {
-            CurrentHealth -= _amt;
+            // prevent negative health being removed.
+            _amt = Mathf.Abs(_amt);
+
+            if(canTakeDamage)
+            {
+                if(damageCooldown > 0.0f)
+                {
+                    canTakeDamage = false;
+                }
+
+                CurrentHealth -= _amt;
+            }
         }
     }
 }
