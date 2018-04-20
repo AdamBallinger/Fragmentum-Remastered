5141b8025e8e4c53bf44a2a06a7b442c74ad52db
Adam Ballinger <crunchienut20@gmail.com>
2018-04-20 17:02:52 +0100

    Added base mushroom minion controller.

---
+++ Assets/Scripts/AI/Controllers/Minions/MushroomMinionAIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scripts.AI.Controllers.Minions
+{
+    public class MushroomMinionAIController : AIController
+    {
+        private Transform player;
+
+        private void OnEnable()
+        {
+            player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        }
+
+        protected override void ControllerUpdate()
+        {
+            RotateTowards(player.position);
+        }
+
+        public override void OnDeath()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
