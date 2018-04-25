49e83b14c0a14e15dfed3e75df2058cd4833434b
Adam Ballinger <crunchienut20@gmail.com>
2018-04-25 14:56:57 +0100

    Added AI Chase Action.

---
+++ Assets/Scripts/AI/ChaseAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scripts.AI
+{
+    public class ChaseAction : AIAction
+    {
+        private Transform target, transform;
+
+        private float speed;
+
+        public ChaseAction(AIActionManager _actionManager, Transform _target, float _speed) : base(_actionManager)
+        {
+            target = _target;
+            transform = ActionManager.Controller.transform;
+            speed = _speed;
+        }
+
+        public override void Update()
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                new Vector3(target.position.x, transform.position.y, target.position.z), speed * Time.deltaTime);
+        }
+    }
+}
