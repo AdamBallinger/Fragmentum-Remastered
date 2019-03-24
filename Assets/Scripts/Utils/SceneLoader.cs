7a91b9509e19a7c89685605aa88ce440826e479f
Adam Ballinger <crunchienut20@gmail.com>
2019-03-24 10:53:20 +0000

    Year old commit. Don't know whats what

---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scripts.Utils
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [Tooltip("Name of the scene to load.")]
+        public string sceneName;
+        
+        public void LoadScene(float _delay = 0.0f)
+        {
+            if (_delay < 0.0f) _delay = 0.0f;
+            
+            Invoke(nameof(Load), _delay);
+        }
+        
+        private void Load()
+        {          
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
